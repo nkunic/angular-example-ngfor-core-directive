48a29cebf6379cb28a5940098099cbcfb391c643
nkunic <45877400+nkunic@users.noreply.github.com>
2023-01-15 22:07:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-example-ngfor-core-directive
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-example-custom-browser-events-nk-wmuh71)
---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter } from '@angular/core';
+import { COURSES } from '../db-data';
+import { Course } from './model/course';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  coreCourse = COURSES[0];
+  rxjsCourse = COURSES[1];
+  ngrxCourse = COURSES[2];
+
+  onCustomEventClicked(course: Course) {
+    console.log(
+      'App component - custom browser click event bubbled...',
+      course
+    );
+  }
+}
